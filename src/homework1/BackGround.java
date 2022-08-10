72adb0679de9d778eebc60abbe9b27efc81e9496
dewinor <kanopus05@mail.ru>
2022-08-10 13:30:53 +0700

    Исправлен класс Background

--- src/homework1/BackGround.java
+++ src/homework1/BackGround.java
@@ -10,12 +10,15 @@ public class BackGround extends Sprites {
      */
 
     Random random = new Random();
-
+    private float range = 0.15f; // min = 0; max = 0.2;
     private Color color;
 
     @Override
     void update(GameCanvas canvas, float deltaTime) {
-        color = new Color(random.nextInt());
+        if (deltaTime < 0.1) {
+            deltaTime *= 10;
+        }
+        color = new Color(deltaTime + random.nextFloat(range), deltaTime + random.nextFloat(range), deltaTime + random.nextFloat(range));
     }
 
     @Override
